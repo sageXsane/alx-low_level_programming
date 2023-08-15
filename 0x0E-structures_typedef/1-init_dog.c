18c4af776b294b0b0f01b65b19df7eec2309140d
Sanelisiwe <sanelisiwebhengu01@gmail.com>
2023-08-14 17:10:15 -0700

    Fix task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - initializes the attributes of d with these attributes
@@ -10,7 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+	{
+		d = malloc(sizeof(dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }

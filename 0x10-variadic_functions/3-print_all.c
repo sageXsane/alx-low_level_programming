15add0817deb1f8635a27ed63687a4587bf2fdb1
Sanelisiwe <sanelisiwebhengu01@gmail.com>
2023-08-17 16:44:46 -0700

    Add task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,66 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints unknown arguments of function of diferent datatypes
+ * @format: string containing what datatype must be printed
+ * @...: arguments
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arg_ptr;
+	int i = 0, arg_count = 0;
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(arg_ptr, format);
+	while (format[i] != '\0')
+	{
+		format_print(arg_ptr, format[i];
+		if ((format[i + 1] != '\0') &
+(format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+		printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(arg_ptr);
+}
+
+/**
+ * format_print - uses switch statements to print the correct datatype
+ * @arg_ptr: pointer to function arguments
+ * @s: the char in format string
+ *
+ */
+void format_print(va_list arg_ptr, char s)
+{
+	char *str;
+
+	switch (c)
+	{
+		case 'c':
+			printf("%c", va_arg(arg_ptr, int));
+			arg_count++;
+			break;
+		case 'i':
+			printf("%d", va_arg(arg_ptr, int));
+			arg_count++;
+			break;
+		case 'f':
+			printf("%f", va_arg(arg_ptr, double));
+			arg_count++;
+			break;
+		case 's':
+			str = va_arg(arg_ptr, char *);
+			if (str == NULL)
+			str = "(nil)";
+			printf("%s", str);
+			arg_count++;
+		break;
+		default:
+			break;
+	}
+

118baa7ff9bb4e0973d267311714ee83a09b9432
Sanelisiwe <sanelisiwebhengu01@gmail.com>
2023-08-17 14:11:18 -0700

    Fix header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,20 +11,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct formatfunc - matches single-character string with related function
- *
- * @s: single-charater string
- * @f: associated function
- */
-struct formatfunc
-{
-	char *s;
-	void (*f)(va_list);
-}
-
-void return_c(va_list);
-void return_i(va_list);
-void return_f(va_list);
-void return_s(va_list);
 #endif

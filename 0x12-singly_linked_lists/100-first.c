9111b2170992c6fd26cf25cbd1e11fcac7f67981
sageXsane <sanelisiwebhengu01@gmail.com>
2023-08-25 18:01:08 -0700

    Make task 100 Betty compliant

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
+/**
+ * first - prints something before the main function is executed
+ */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

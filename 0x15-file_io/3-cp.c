c78dbf6d66a8f7e16787c59527cf8b97ec026f61
sageXsane <sanelisiwebhengu01@gmail.com>
2023-09-08 20:43:55 -0700

    Add subfunction in task 3, update main.h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,44 +21,55 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-| S_IRGRP | S_IWGRP | S_IROTH);
+	if ((av[1] == NULL) || (strcmp(av[1], "\0") == 0))
+		file_error(av[1], 0);
+	if ((av[2] == NULL) || (strcmp(av[2], "\0") == 0))
+		file_error(av[2], 1);
 	fd_frm = open(av[1], O_RDONLY);
 	if (fd_frm < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		file_error(av[1], 0);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+| S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-		exit(99);
-	}
-	rd_count = read(fd_frm, buffer, sizeof(buffer));
+		file_error(av[2], 1);
+	rd_count = read(fd_frm, buffer, 1024);
 	while (rd_count > 0)
 	{
 		wr_count = write(fd_to, buffer, rd_count);
 		if (wr_count < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-			exit(99);
-		}
-		rd_count = read(fd_frm, buffer, sizeof(buffer));
+			file_error(av[2], 1);
+		rd_count = read(fd_frm, buffer, 1024);
 	}
 	if (rd_count < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (close(fd_frm) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_frm);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		file_error(av[1], 0);
+	close_error(fd_frm);
+	close_error(fd_to);
 	return (0);
+}
+
+/**
+ * file_error - prints relavant file errors to stderr
+ * @filename: name of file
+ * @location: determines which file we are checking. from file = 0, to file = 1
+ */
+void file_error(char *filename, int location)
+{
+	if (location == 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	else
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
+	exit(98 + location);
+}
+
+/**
+ * close_error - pritns the relevant error msg when closing a file
+ * @fd: the file descriptor of file
+ */
+void close_error(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void file_error(char *filename, int location);
+void close_error(int fd);
 #endif

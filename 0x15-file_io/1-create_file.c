0f8828654fcf383a298a19a67daa9590e8b60f3b
sageXsane <sanelisiwebhengu01@gmail.com>
2023-09-08 17:15:39 -0700

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a new fileand writes a string received from cmmdline
  * @filename: the name of the file
- * @text_context: the string that must be written to file
+ * @text_content: the string that must be written to file
  *
  * Description: The created file should have read/write permissions else
  * the permissions of the existing file should remain unchanged but file will
@@ -13,16 +13,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t bytes;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	if (text_content != NULL)
 	{
-		if (write(fd, text_content, strlen(text_content))  == -1)
+		bytes = write(fd, text_content, strlen(text_content));
+		if ((bytes < 0) || ((size_t)bytes < strlen(text_content)))
+		{
+			close(fd);
 			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
